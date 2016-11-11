389541ed4c5a43b0538b5d82a217fcf73f41cda1
Bas Broek <bas@basbroek.nl>
2016-11-11 19:27:16 +0100

    Remove pods from sample project

--- Example/Pods/Target Support Files/Analysis/Analysis-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Analysis : NSObject
-@end
-@implementation PodsDummy_Analysis
-@end
--- Example/Pods/Target Support Files/Analysis/Analysis-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double AnalysisVersionNumber;
-FOUNDATION_EXPORT const unsigned char AnalysisVersionString[];
-
