bd64d3ba60b1383ba9350bcc4781f31800e15d68
Bas Broek <bas@basbroek.nl>
2016-12-09 12:14:40 +0100

    Add tests for hashValue

--- Example/Tests/AnalysisTests.swift
+++ Example/Tests/AnalysisTests.swift
@@ -205,4 +205,11 @@ class AnalysisTests: XCTestCase {
     XCTAssertEqual(a.debugDescription, dump(a.description))
     XCTAssertEqual(z.debugDescription, dump(z.description))
   }
+  
+  func testHashValue() {
+    XCTAssertEqual(helloWorld1.hashValue, helloWorld1.input.hashValue)
+    XCTAssertEqual(helloWorld2.hashValue, helloWorld2.input.hashValue)
+    XCTAssertEqual(a.hashValue, a.input.hashValue)
+    XCTAssertEqual(z.hashValue, z.input.hashValue)
+  }
 }
